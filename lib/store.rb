06d696291b83a23704bb1419960f8101f1d2e47a
kaylavu <kaylaavu@gmail.com>
2018-07-17 20:31:37 +0000

    exercises 1-7

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,16 @@
 class Store < ActiveRecord::Base
 
+    has_many :employees
+    validates :name, presence: true, length: {minimum: 3}
+    validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+    validate :carry_men_or_womens
+
+    def carry_men_or_womens
+        if !mens_apparel.present? && !womens_apparel.present? 
+          errors.add(:store, "Must carry at least men or womens")
+        end
+    end
+
 end
+
+
